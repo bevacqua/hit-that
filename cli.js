d3c123591666cfee3f293cca6c186a6b53f9b393
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2015-04-10 19:55:45 -0300

    relative

--- cli.js
+++ cli.js
@@ -5,7 +5,7 @@
 var fs = require('fs');
 var meow = require('meow');
 var hitThat = require('./');
-var usage = fs.readFileSync('./usage.txt');
+var usage = fs.readFileSync(__dirname + '/usage.txt');
 var cli = meow({ help: usage });
 var url = cli.input.shift();
 var options = cli.flags;
