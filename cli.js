3affd31b2a3b3f1450dafb0e0456dfd71aa96c3f
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2015-10-08 11:16:28 -0300

    meow introduced a non-semver breaking change

--- cli.js
+++ cli.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 var meow = require('meow');
 var hitThat = require('./');
 var usage = fs.readFileSync(__dirname + '/usage.txt');
-var cli = meow({ help: usage });
+var cli = meow(usage);
 var url = cli.input.shift();
 var options = cli.flags;
 hitThat(url, options);
